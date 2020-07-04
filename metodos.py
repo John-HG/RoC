9bf2488083a679e54d39565b0827fc7eaa6fe5b4
Enrique Rios <enriquemanuelriosperez@gmail.com>
2020-07-04 10:59:14 -0500

    Programa funcianal sin login

--- metodos.py
+++ metodos.py
@@ -7,6 +7,7 @@ import serial
 
 
 
+
 #Creamos el data y el grafo con el que vamos a trabajar 
 def db():
     data = pd.read_csv("C:/Users/Enrique Manuel/Desktop/multiventanas/base_datos.csv")        #lee la base de datos 
@@ -37,27 +38,6 @@ def busqueda_error(star, stop, error):
                     
     return astar                                        
 
-def direccion_distancica(lista):
-    x = None
-    data, g = db()                  
-    for i in range(len(lista)-1):                       #obtener el numero de nodos de la lista
-        origen = lista[i]                               #de la lista obtenemos el origen "virtual" por que siempre cambia 
-        destino = lista[i+1]                            #de la lista obtenemos el destino "virtual"
-        datos = data[(data["origen"] == origen) & (data["destino"] == destino)] #comparamos en el db que el inicio y el fin son los deseados
-        di = datos["direccion"]                         #obtenemos la direccion de ese inicio y fin 
-        metros = g[origen][destino]["large"]            #obtenemos la distancia de el grafo entre destino y origen
-        for i in di:                    
-            x = i #x.append(i)
-        if x == 1:
-            lado = x#"derecha"
-        elif x == 2:
-            lado = x#"arriba"
-        elif x == 3:
-            lado = x#"izquierda"
-        elif x == 4:
-            lado = x #"abajo"
-        print("La distancia de {} a {} es {} y hacia {} \n".format(origen, destino, metros, lado))
-
 #confirmamos la existencia del nodo en la db
 def cofirmarnodo(nodo):
     data, _ = db()                              #obtenemos el db de la funcion db()
@@ -68,6 +48,7 @@ def cofirmarnodo(nodo):
     else:
         return True
 
+#mandamos el mensaje de confirmacion 
 def mensaje_nodo(o,d):
     lista =[]
     for i in(o,d):
@@ -81,56 +62,39 @@ def mensaje_nodo(o,d):
     print(lista)
     if lista[0]== True and lista[1] == True:
         lis = busqueda(o,d)
-        direccion_distancica(lis)
+        #direccion_distancica(lis)
         mensaje= "Nodos Correctos"
         return mensaje
     else:
         mensaje = "Uno o mas nodos no estan en el db"
-        
-
-def verificacion(punto):
-    espera = 10
-    data, g = db()
-    todos_los_nodos= g.nodes()
-    t = time.time()  # ingresamos el tiempo base
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)#abrimos la camara
-    #font = cv2.FONT_HERSHEY_PLAIN   // por si queremos mostrar algun mensaje en la pantalla de la camara
-    #qr = [] #es un un arreglo vaicio el cual ya a contener la salida de la funcion
-    estado = True #es la llave para el ciclo while creo que se puede cambiar y solamente poner while true y poner breaks
-    time.sleep(1)
-    while estado:
-        n = time.time() #obtenemos el tiempo
-        _, frame = cap.read() #creamos la variable frame la cual nos va a dar la lectura de la camara
-        decodedObjects = pyzbar.decode(frame) #aqui obtenemos el objeto qr el cual lo va a obtener de la imagen obtenida frame
-        for obj in decodedObjects:
-            (x, y, w, h) = obj.rect #obtenemos las coordenadas del objeto
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2) #dibujamos un rectangulo en las coordenadas obtenidas
-            barcodeData = obj.data.decode("utf-8") #obtenemos el mensaje que contine la imagen qr
-            #cv2.putText(frame, str(barcodeData), (30, 30), font, 2, (255, 0, 0), 3)
-            for i in todos_los_nodos: #recorre la variable i en la lista de nodos
-                if barcodeData == i:
-                    qr = barcodeData
-                    estado = False
-             #print("Type: " , obj.type)
-        cv2.imshow("Frame", frame)
-        if (n== t + espera) or (n>= t + espera): #aquie se genera la cuenta
-            estado = False
-        #else:
-        #    print("{}".format(n-t)) # imprime el tiempo que va pasando
-        key = cv2.waitKey(3) & 0xff
-        if key == False:
-            break
-    cap.release()
-    cv2.destroyAllWindows()
-
-    if len(qr) != 0: # aqui manda el estado y el qr
-        if qr == punto:
-            return True, qr #si qr es igual al punto que mandamos envia un true y el punto
-        else:
-            return False, qr # si no manda un falso pero si lee algo y ese algo lo manda
-    else:
-        return False, None  # si no lee nada manda un falso y una lista vacia
-
-
-
            
+#obtenemos la direccion 
+def direccion(origen,destino):
+    x = None
+    data, g = db()                  
+    datos = data[(data["origen"] == origen) & (data["destino"] == destino)] #comparamos en el db que el inicio y el fin son los deseados
+    di = datos["direccion"]                         #obtenemos la direccion de ese inicio y fin 
+    metros = g[origen][destino]["large"]            #obtenemos la distancia de el grafo entre destino y origen
+    for i in di:                    
+        x = i #x.append(i)
+    if x == 1:
+        lado = x#"derecha"
+    elif x == 2:
+        lado = x#"arriba"
+    elif x == 3:
+        lado = x#"izquierda"
+    elif x == 4:
+        lado = x #"abajo"
+    y = "La distancia de {} a {} es {} y hacia {} \n".format(origen, destino, metros, lado)
+    return y,x
+
+def comunicacion_arduino(nodo):
+    arduino = serial.Serial("COM4", 9600)
+    time.sleep(2)
+    arduino.write(nodo.encode())
+    time.sleep(.5)
+    arduino.close()
+
+
+
+#y = direccion("A","B")
